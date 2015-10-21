f53506631e0661aa3f26f6de48f2a2ff9d8bd769
jesusdrodriguez <jesus.d.rodriguez01@utrgv.edu>
2015-10-21 15:10:16 -0500

    v1

---
+++ DiamondKing_SurvivalGame/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public int speed;
+    public Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        AI_movement();
+	}
+
+    void AI_movement() {
+
+        //transform.position = transform.forward * speed * Time.deltaTime;
+    }
+}
+
---
+++ DiamondKing_SurvivalGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    public bool isGrounded;
+    Vector2 movement;
+    Animator anim;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        isGrounded = true;
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+        
+        Movement();
+	}
+
+    void Movement()
+    {
+        anim.SetFloat("speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+
+        if(Input.GetAxisRaw("Horizontal") > 0) // right
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0) // left
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector2.up * speed * 40);
+            isGrounded = false;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            //crouch
+        }
+    }
+    void OnCollisionEnter(Collision2D other){
+
+        Debug.Log("......");
+
+        if(other.gameObject.tag == "Terrain")
+            isGrounded = true;
+    }
+
+}
